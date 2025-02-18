617f2dc6937c065dc00c63be2cef2be14b1ce7aa
FranciscoLi2000 <francisco2000li@icloud.com>
2025-02-18 17:01:41 +0000

    easier way to understand

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -1,5 +1,5 @@
 #include "libft.h"
-
+/*
 static t_list	*create_new_node(void *content, void (*del)(void *))
 {
 	t_list	*new_node;
@@ -34,5 +34,33 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		ft_lstadd_back(&new_list, new_node);
 		lst = lst->next;
 	}
+	return (new_list);
+}
+*/
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new_list;
+	t_list	*new_node;
+
+	new_list = NULL;
+	/* check if lst or f or del is NULL */
+	if (!lst || !f || !del)
+		return (NULL);
+	/* loop over lst */
+	while (lst)
+	{
+		/* create a new element */
+		new_node = ft_lstnew(f(lst->content));
+		/* if new elem is null, clear the new list */
+		if (!new_node)
+		{
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+		/* add the new element to the back of the list */
+		ft_lstadd_back(&new_list, new_node);
+		lst = lst->next;
+	}
+	/* finally, return the new list */
 	return (new_list);
 }
