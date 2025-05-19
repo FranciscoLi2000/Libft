183ab797262c32a9953a87e60a74fc94fcafec45
FranciscoLi2000 <liyufeng577@gmail.com>
2025-05-19 09:36:20 +0200

    exam

--- exam_rank_2/ft_putstr.c
+++ exam_rank_2/ft_putstr.c
@@ -1,4 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yufli <yufli@student.42barcelona.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/19 09:07:03 by yufli             #+#    #+#             */
+/*   Updated: 2025/05/19 09:08:25 by yufli            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "exam.h"
 
-void	ft_putstr(char *str)
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
 
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]); 
+		i++;
+	}
+}
--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,99 +6,61 @@
 /*   By: yufli <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 11:37:00 by yufli             #+#    #+#             */
-/*   Updated: 2025/01/02 18:44:28 by yufli            ###   ########.fr       */
+/*   Updated: 2025/05/19 09:34:14 by yufli            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdlib.h>
 #include "libft.h"
 
-char	**ft_split(char const *s, char c);
-
-static char	**allocate_substrings(char const *s, char c, int *count)
-{
-	char	**result;
-	int		i;
-
-	*count = 0;
-	i = 0;
-	while (s[i] != '\0')
-	{
-		while (s[i] == c)
-			i++;
-		if (s[i] != '\0')
-		{
-			(*count)++;
-			while (s[i] != '\0' && s[i] != c)
-				i++;
-		}
-	}
-	result = malloc((*count + 1) * sizeof(char *));
-	if (!result)
-		return (NULL);
-	return (result);
-}
-
-static int	get_wordlen(const char *s, char c, int start)
+static int	word_len(char const *s, char c)
 {
 	int	len;
 
 	len = 0;
-	while (s[start + len] != '\0' && s[start + len] != c)
+	while (*s && *s == c)
+	{
 		len++;
+		s++;
+	}
 	return (len);
 }
 
-static int	store_substrings(char const *s, char c, char **result)
+static void	free_all(char **res, int i)
 {
-	int	i;
-	int	j;
-
-	i = 0;
-	j = 0;
-	while (s[i] != '\0')
-	{
-		while (s[i] == c)
-			i++;
-		if (s[i] != '\0')
-		{
-			result[j] = ft_substr(s, i, get_wordlen(s, c, i));
-			if (!result[j])
-				return (j);
-			j++;
-			while (s[i] != '\0' && s[i] != c)
-				i++;
-		}
-	}
-	result[j] = NULL;
-	return (-1);
-}
-
-static void	free_split_memory(char **result, int j)
-{
-	while (j >= 0)
-	{
-		free(result[j]);
-		j--;
-	}
-	free(result);
+	while (--i >= 0)
+		free(res[i]);
+	free(res);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**result;
-	int		failed_index;
 	int		count;
+	int		i;
+	int		len;
 
-	result = allocate_substrings(s, c, &count);
-	if (!s || !result)
+	count = 0;
+	i = 0;
+	len = 0;
+	if (!s)
 		return (NULL);
-	failed_index = store_substrings(s, c, result);
-	if (failed_index != -1)
+	while (s[len])
 	{
-		free_split_memory(result, failed_index - 1);
+		if (s[len++] != c && (len == 1 || s[len - 2] == c))
+			count++;
+	}
+	result = malloc(sizeof(char *) * (count + 1));
+	if (!result)
 		return (NULL);
+	while (*s)
+	{
+		while (*s == c)
+			s++;
+		if (*s)
+		{
+			len = word_len(s, c);
+			result[i] = ft_substr(s, 0, len);
+		}
 	}
 	return (result);
 }
