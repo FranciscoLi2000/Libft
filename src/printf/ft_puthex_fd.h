78223e4caf6c0095e6985b304635e3267170b330
FranciscoLi2000 <liyufeng577@gmail.com>
2026-02-24 01:20:37 +0000

    remake all

--- src/printf/ft_puthex_fd.h
+++
@@ -1,6 +0,0 @@
-#ifndef FT_PUTHEX_FD_H
-# define FT_PUTHEX_FD_H
-
-void ft_puthex_fd(unsigned long n, int fd, int upper);
-
-#endif
--- src/printf/ft_putunbr_fd.h
+++
@@ -1,6 +0,0 @@
-#ifndef FT_PUTUNBR_FD_H
-# define FT_PUTUNBR_FD_H
-
-void ft_putunbr_fd(unsigned int n, int fd);
-
-#endif
